51887a66e3d26d03bdcc21195141ba7b7aeeed77
Giggle <2386181363@qq.com>
2024-12-11 08:38:39 +0000

    [lab3]init lab3(some code formatted)

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # 定义目标
 # lab2
-.PHONY: stress-test compile-code unit-test clear
+.PHONY: stress-test compile-code unit-test clear compile
 compile-code:
 	mkdir -p build && cd build && cmake .. && make -j && make build-tests -j
 
@@ -12,3 +12,6 @@ stress-test:
 
 clean:
 	rm -f /tmp/inode_file /tmp/test_file1 /tmp/test_file2 /tmp/test_file3 /tmp/test_file 
+
+compile:
+	mkdir -p build && cd build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
--- src/include/rsm/raft/protocol.h
+++ src/include/rsm/raft/protocol.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "rsm/raft/log.h"
 #include "rpc/msgpack.hpp"
+#include "rsm/raft/log.h"
 
 namespace chfs {
 
@@ -18,70 +18,77 @@ const std::string RAFT_RPC_APPEND_ENTRY = "append entries";
 const std::string RAFT_RPC_INSTALL_SNAPSHOT = "install snapshot";
 
 struct RequestVoteArgs {
-    /* Lab3: Your code here */
-    
-    MSGPACK_DEFINE(
-    
-    )
+  /* Lab3: Your code here */
+  int term;         // candidate’s term
+  int candidateId;  // candidate requesting vote
+  int lastLogIndex; // index of candidate’s last log entry
+  int lastLogTerm;  // term of candidate’s last log entry
+  RequestVoteArgs(int term, int candidateId, int lastLogIndex, int lastLogTerm)
+      : term(term), candidateId(candidateId), lastLogIndex(lastLogIndex),
+        lastLogTerm(lastLogTerm) {}
+  RequestVoteArgs() {}
+  MSGPACK_DEFINE(term, candidateId, lastLogIndex, lastLogTerm)
 };
 
 struct RequestVoteReply {
-    /* Lab3: Your code here */
-
-    MSGPACK_DEFINE(
-    
-    )
+  /* Lab3: Your code here */
+  int term;         // currentTerm, for candidate to update itself
+  bool voteGranted; // true means candidate received vote
+  int followerId;
+  RequestVoteReply(int term, bool voteGranted, int followerId)
+      : term(term), voteGranted(voteGranted), followerId(followerId) {}
+  RequestVoteReply() {}
+  MSGPACK_DEFINE(term, voteGranted, followerId)
 };
 
-template <typename Command>
-struct AppendEntriesArgs {
-    /* Lab3: Your code here */
+template <typename Command> struct AppendEntriesArgs {
+  /* Lab3: Your code here */
 };
 
 struct RpcAppendEntriesArgs {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(
+
+  )
 };
 
 template <typename Command>
-RpcAppendEntriesArgs transform_append_entries_args(const AppendEntriesArgs<Command> &arg)
-{
-    /* Lab3: Your code here */
-    return RpcAppendEntriesArgs();
+RpcAppendEntriesArgs
+transform_append_entries_args(const AppendEntriesArgs<Command> &arg) {
+  /* Lab3: Your code here */
+  return RpcAppendEntriesArgs();
 }
 
 template <typename Command>
-AppendEntriesArgs<Command> transform_rpc_append_entries_args(const RpcAppendEntriesArgs &rpc_arg)
-{
-    /* Lab3: Your code here */
-    return AppendEntriesArgs<Command>();
+AppendEntriesArgs<Command>
+transform_rpc_append_entries_args(const RpcAppendEntriesArgs &rpc_arg) {
+  /* Lab3: Your code here */
+  return AppendEntriesArgs<Command>();
 }
 
 struct AppendEntriesReply {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(
+
+  )
 };
 
 struct InstallSnapshotArgs {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(
+
+  )
 };
 
 struct InstallSnapshotReply {
-    /* Lab3: Your code here */
+  /* Lab3: Your code here */
 
-    MSGPACK_DEFINE(
-    
-    )
+  MSGPACK_DEFINE(
+
+  )
 };
 
 } /* namespace chfs */
